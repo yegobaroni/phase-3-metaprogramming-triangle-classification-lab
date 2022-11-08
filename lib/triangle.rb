fa79d43f784dc151edb1baabcc8439faf41224d6
yego <yegokipkemoi254@gmail.com>
2022-11-08 23:50:00 +0300

    commit tree f39f530c9f7f369697bf2082453e101f3d90714a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1      
+    @side2 = side2      
+    @side3 = side3 
+  end
+
+  def valid_num?
+    [@side1, @side2, @side3].all? { |num| num > 0}
+  end
+
+  def valid_triangle?
+    @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side1 + @side3 > @side2
+  end
+
+  def kind
+    try_error 
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 ||  @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def try_error
+    raise TriangleError unless valid_num? && valid_triangle?
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
